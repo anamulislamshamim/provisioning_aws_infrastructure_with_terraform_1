dfb98133d1e26a2308f9620141fc91afe7eb216f
Shamim <108428223+anamulislamshamim@users.noreply.github.com>
2025-05-23 23:13:30 +0600

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,24 @@
+# CIDR = Class less Inter Domain Routing
+variable "vpc_cidr" {
+  type        = string
+  description = "Public Subnet CIDR values"
+  default     = "10.0.0.0/16"
+}
+
+variable "cidr_public_subnet" {
+  type        = list(string)
+  description = "Public Subnet CIDR values"
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "cidr_private_subnet" {
+  type        = list(string)
+  description = "Private Subnet CIDR values"
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "asia_availability_zone" {
+  type        = list(string)
+  description = "Availability Zones"
+  default     = ["ap-south-1a", "ap-south-1b"]
+}
